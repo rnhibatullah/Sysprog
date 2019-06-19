5c76700296011c2afb06b1f7fbffe1437d6384c5
lisa_bronowsky <lisa.bronowsky@gmail.com>
2019-06-19 13:48:50 +0200

    Update to Praxis

---
+++ Blatt04/Praxis/Vorgabe/src/management.c
@@ -0,0 +1,155 @@
+#include "./../include/management.h"
+#include "./../include/order.h"
+#include "./../include/supplier.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+
+
+struct management{
+    //TODO
+    //ordes_in is a ringbuffer where the incomming orders are placed by the management
+    order_list* orders_in;
+    
+    //orders_finished is an array where finished orders are placed by the workers
+    order* orders_finished[MAX_ORDERS];
+
+    //Mutex for orders in
+    //mutex ordersIn_m;
+
+    unsigned int num_worker_threads;
+    FILE* order_file;
+    forwarding_agent* fa;
+    
+};
+
+management* management_create(char* filename, unsigned int num_worker_threads, forwarding_agent* fa){
+    //TODO
+    //Lock Needed
+    //lock(ordersIn_m);
+
+    management* new_management = malloc(sizeof(management));
+    if(new_management == NULL)
+        return NULL;
+    
+    new_management->orders_in = order_list_create();
+    if(new_management->orders_in == NULL)
+        return NULL;
+
+    //Unlock
+    //unlock(ordersIn_m);
+
+    for(int i = 0; i<MAX_ORDERS;++i){
+        new_management->orders_finished[i] = NULL;
+    }
+
+    
+    new_management->num_worker_threads = num_worker_threads;
+    
+    new_management->order_file = fopen(filename,"r");
+    if(new_management->order_file == NULL){
+        printf("could not open %s\n",filename);
+        return NULL;
+    }
+    
+    new_management->fa = fa;
+    
+   
+    return new_management;
+}
+
+//THREAD FUNCTION 
+//reads new orders from file and deposit orders in the orders_in ringbuffer
+void* get_and_deposit_new_orders(void* man){
+    management* m = (management*) man;
+    char buffer[100];
+    char* client;
+    unsigned int muffin;
+    int count_orders = 0;
+    while(fgets(buffer,100,m->order_file) && count_orders <MAX_ORDERS){
+        client = strtok(buffer,";\n");
+        
+        if(client == NULL){
+            break;
+        }
+        
+        muffin = atoi(strtok(NULL,";\n"));
+        if(muffin != 0){
+            order* new_order = order_create(client, muffin);
+            ++count_orders;
+            deposit_order(m->orders_in, new_order);
+        }
+       
+    }
+    
+    // ADD NULL at the end of the buffer. NULL is the terminating signal for the workers
+    for(unsigned int i = 0; i<m->num_worker_threads;++i){
+        //++count;
+        deposit_order(m->orders_in,NULL);
+    }
+    
+    return NULL;
+}
+
+//FUNCTIONS CALLED BY WORKER THREADS
+//worker threads deposit the order they finished in the orders_finished array
+void deposit_finished_orders(management* m, order* o){
+    //TODO
+   
+    int i = 0;
+    while(i< MAX_ORDERS){
+        if(m->orders_finished[i] == NULL){
+            m->orders_finished[i] = o;
+            return;
+        }
+        ++i;
+    }
+    
+    //to many orders have been executed
+    order_destroy(o);    
+}
+ 
+//returns the next order in the ringbuffer
+
+    //lock(ordersIn_m);
+order* get_order_from_management(management* m){   
+    order* o = get_order(m->orders_in);
+    //unlock(ordersIn_m);
+
+    return o;
+}
+
+//FUNCTION CALLED BY BAKERY
+unsigned int management_destroy(management* m){  
+    //TODO
+        stop_forwarding_agent(m->fa);
+
+        //lock(ordersIn_m);
+        while(get_count(m->orders_in) > 0){
+            order* tmp = get_order(m->orders_in);
+            order_destroy(tmp);
+        }
+        order_list_destroy(m->orders_in);
+        //unlock(ordersIn_m);
+
+        //Printing summaray
+        unsigned int sum_of_muffin = 0;
+        for(int i = 0; i<MAX_ORDERS && m->orders_finished[i] != NULL;++i){
+            printf("client: %s \t muffin ordered: %u\n",get_client(m->orders_finished[i]), get_muffin(m->orders_finished[i]));
+            sum_of_muffin += get_muffin(m->orders_finished[i]);
+            order_destroy(m->orders_finished[i]);
+        }
+        printf("sum of muffin: %u\n", sum_of_muffin);
+        
+        fclose(m->order_file);
+        
+        free(m);
+        return sum_of_muffin;
+    }
+
+
+order_list* get_orders_in(management* m){
+    return m->orders_in;
+}
+    
---
+++ Blatt04/Praxis/Vorgabe/src/warehouse.c
@@ -0,0 +1,129 @@
+#include "./../include/warehouse.h"
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+struct warehouse{
+    unsigned int flour, sugar, choclate;
+    int locked;
+    
+    unsigned int sugar_taken, flour_taken, choclate_taken;
+    //TODO
+
+    
+};
+
+warehouse* warehouse_create(){
+    warehouse* new_warehouse = (warehouse*) malloc(sizeof(warehouse));
+    if(new_warehouse== NULL){
+        printf("Unable to allocate memory in function %s()\n",__func__);
+        return NULL;
+    }
+    new_warehouse->flour = 0;
+    new_warehouse->sugar = 0;
+    new_warehouse->choclate = 0;
+    new_warehouse->locked = 0;
+
+    //signal()
+    // wait()
+    //TODO
+    
+    new_warehouse->choclate_taken = 0;
+    new_warehouse->flour_taken = 0;
+    new_warehouse->sugar_taken = 0;
+    return new_warehouse;
+}
+
+//FUNCTIONS CALLED BY WORKER THREADS
+//@param: amount of flour/sugar/choclate to be taken from warehouse wh
+
+unsigned int get_choclate(unsigned int amount, warehouse* wh){
+    
+    wh->choclate_taken += amount;
+    wh->choclate -= amount;
+    
+    
+    return amount;
+}
+
+unsigned int get_flour(unsigned int amount, warehouse* wh){
+    
+    //TODO
+    
+    wh->flour_taken += amount;
+    wh->flour -= amount;
+    
+    return amount;
+}
+
+unsigned int get_sugar(unsigned int amount, warehouse* wh){
+    
+    //TODO
+    
+    wh->sugar_taken += amount;
+    wh->sugar -= amount;
+        
+    return amount;
+}
+
+
+//FUNCTIONS CALLED BY SUPPLIER THREADS
+//@param: amount of flour/sugar/choclate to be store in warehouse wh
+void deposit_choclate(unsigned int amount, warehouse* wh){
+    //TODO
+    
+    if(wh->choclate + amount > MAX_STORE_CAPACITY){
+        wh->choclate = MAX_STORE_CAPACITY;
+    }else{
+        wh->choclate += amount;
+    }
+    
+    
+    return;
+}
+
+void deposit_sugar(unsigned int amount, warehouse* wh){
+    //TODO
+    
+    if(wh->sugar + amount > MAX_STORE_CAPACITY){
+        wh->sugar = MAX_STORE_CAPACITY;
+    }else{
+        wh->sugar += amount;
+    }
+    
+    return;
+}
+
+void deposit_flour(unsigned int amount, warehouse* wh){
+    //TODO
+    
+    if(wh->flour + amount > MAX_STORE_CAPACITY){
+        wh->flour = MAX_STORE_CAPACITY;
+    }else{
+        wh->flour += amount;
+    }
+    
+    return;
+}
+
+
+//FUNCTIONS CALLED BY BAKERY
+void lock_warehouse(warehouse* wh){
+    //TODO
+    wh->locked=1;
+
+}
+
+unsigned int get_used_resources(warehouse* wh, int type){
+    if(type == SUGAR) return wh->sugar_taken;
+    if(type == FLOUR) return wh->flour_taken;
+    if(type == CHOCLATE) return wh->choclate_taken;
+    return 0;
+}
+
+
+void warehouse_destroy(warehouse* wh){
+    //TODO
+    free(wh);
+    return;
+}
